6f3efa4088db5bf1d64ef61ccebbe872dba66fb1
Nagasai0102 <madalanagasai@gmail.com>
2022-02-09 11:39:57 +0530

    1

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ def print_hi(name):
     print(f'Hi, {name}')
 def add(x,y):
     print(x+y)
-
+def sub(x,y):
+    print(x-y)
+def mul(x,y):
+    print(x*y)
 
 if __name__ == '__main__':
     print_hi('PyCharm')
     add(2,7)
+    sub(55,5)
+    mul(4,5)
+    print('this is master')
 
