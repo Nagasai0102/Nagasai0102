25f346abcc5bfe2d69f4459e2e25ac24d2521117
Nagasai0102 <madalanagasai@gmail.com>
2022-02-22 15:37:11 +0530

    574

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ def sub(x,y):
     print(x-y)
 def mul(x,y):
     print(x*y)
-
+print("hell no")
 if __name__ == '__main__':
     print_hi('PyCharm')
     add(2,7)
     sub(55,5)
     mul(4,5)
     print('this is master')
-
